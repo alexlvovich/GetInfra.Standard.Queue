3cf3371d0ce39a9e734074a88d6d5c40c7ea21ef
Alexander Lvovich <alex.lvovich@indevlabs.de>
2020-07-24 23:08:44 +0200

    consumer and the publisher

---
+++ src/GetInfra.Standard.Queue/Implementations/ServiceBus/AzureServiceBusPublisher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using GetInfra.Standard.Queue.Model;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GetInfra.Standard.Queue.Implementations.ServiceBus
+{
+    public class AzureServiceBusPublisher : IQueuePublisher
+    {
+        public ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly QueueClient _client;
+        private readonly IJsonSerializer _serializer;
+
+        public AzureServiceBusPublisher(ILoggerFactory loggerFactory, IConfiguration configuration, IJsonSerializer serializer, string publisherName)
+        {
+            _logger = loggerFactory.CreateLogger<AzureServiceBusPublisher>();
+            _serializer = serializer;
+            _configuration = configuration;
+
+            if (publisherName == null)
+            {
+                _logger.LogError("publisher name not specified");
+                throw new ArgumentException("publisher name not specified");
+            }
+
+            var publisher = _configuration.GetSection("AzureServiceBus:" + publisherName).Get<ServiceBusConfig>();
+            if (publisher == null)
+            {
+                _logger.LogError("publisher configuration not found");
+                throw new Exception("publisher configuration not found");
+            }
+
+            var conSting = new ServiceBusConnectionStringBuilder(publisher.Endpoint, publisher.EntityPath, publisher.SasKeyName, publisher.SasKey);
+
+            _client = new QueueClient(conSting);
+        }
+
+        public async Task Enqueue(QMessage msg)
+        {
+            var jsonified = _serializer.Serialize(msg);
+
+            var messageBuffer = Encoding.UTF8.GetBytes(jsonified);
+            await _client.SendAsync(new Message(messageBuffer));
+
+        }
+
+       
+    }
+}
